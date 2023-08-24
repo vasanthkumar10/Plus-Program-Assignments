b0d43bc5500a9c25df4d129263a610d787f7a86f
vasanthkumar10 <vasizebron10@gmail.com>
2023-08-24 22:29:12 +0530

    redux project demo

--- redux/asyncDemo.js
+++ redux/asyncDemo.js
@@ -18,7 +18,7 @@ const FETCH_USERS_REQUEST = "FETCH_USERS_REQUEST";
 const FETCH_USERS_SUCCESS = "FETCH_USERS_SUCCESS";
 const FETCH_USERS_FAILURE = "FETCH_USERS_FAILURE";
 
-// action create
+// action creators
 const fetchUsersRequest = () => {
   return {
     type: FETCH_USERS_REQUEST,
@@ -44,7 +44,7 @@ const fetchUsers = () => {
     dispatch(fetchUsersRequest());
 
     axios
-      .get("https://jsonplaceholder.typicode.com/sers")
+      .get("https://jsonplaceholder.typicode.com/users")
       .then((response) => {
         const users = response.data;
         dispatch(fetchUsersSuccess(users));
